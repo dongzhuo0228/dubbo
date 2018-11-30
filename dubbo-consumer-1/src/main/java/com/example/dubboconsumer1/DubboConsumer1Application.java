630f950a06513c1ac6a644f4d46902095fad437d
董茁 <dongz001928@yimidida.com>
2018-11-30 10:25:41 +0800

    重新提交

--- dubbo-consumer-1/src/main/java/com/example/dubboconsumer1/DubboConsumer1Application.java
+++ dubbo-consumer-1/src/main/java/com/example/dubboconsumer1/DubboConsumer1Application.java
@@ -1,9 +1,11 @@
 package com.example.dubboconsumer1;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class DubboConsumer1Application {
 
     public static void main(String[] args) {
--- dubbo-provider-1/src/main/java/com/example/dubboprovider1/DubboProvider1Application.java
+++ dubbo-provider-1/src/main/java/com/example/dubboprovider1/DubboProvider1Application.java
@@ -1,9 +1,11 @@
 package com.example.dubboprovider1;
 
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubboConfiguration
 public class DubboProvider1Application {
 
     public static void main(String[] args) {
